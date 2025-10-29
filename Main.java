806ab5e7e1ef573f4ea06c86fb4017ffbd513ec5
firdauzy <nuzulafirdhauzy@gmail.com>
2025-10-29 14:50:23 +0700

    testing commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Program perulangan for untuk mencetak angka 1-10
+        System.out.println("Mencetak angka 1 sampai 10:");
+        
+        for(int i = 1; i <= 10; i++) {
+            System.out.println("Angka ke-" + i);
+        }
+    }
+}
