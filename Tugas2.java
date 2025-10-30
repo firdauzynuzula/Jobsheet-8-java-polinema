88469d647348fad9298241c41fa9112cc6ca93cd
firdauzy <nuzulafirdhauzy@gmail.com>
2025-10-30 15:09:59 +0700

    mengubah nama file main java menjadi tugas2.java

--- Tugas2.java
+++ Tugas2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Tugas2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String pilihan;
